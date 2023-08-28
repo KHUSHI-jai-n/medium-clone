f5bb3a4f1c18a77cec58e973c68cef4b0fb735e0
Khushi Jain <khushijain.mp@gmail.com>
2023-08-28 22:19:49 +0530

    Updates

---
+++ precache-manifest.4d50899c198430b89b4404a587f86309.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b26c18c7c2abbe79546",
+    "url": "/medium-clone/static/css/main.53265a5e.chunk.css"
+  },
+  {
+    "revision": "0b26c18c7c2abbe79546",
+    "url": "/medium-clone/static/js/main.0b26c18c.chunk.js"
+  },
+  {
+    "revision": "054b294fadbeae40b577",
+    "url": "/medium-clone/static/js/1.054b294f.chunk.js"
+  },
+  {
+    "revision": "ebc48a2cf82accbf4366",
+    "url": "/medium-clone/static/js/runtime~main.ebc48a2c.js"
+  },
+  {
+    "revision": "a921d0343ae89048e1e3060fa712408b",
+    "url": "/medium-clone/static/media/logo.a921d034.png"
+  },
+  {
+    "revision": "bff46db74b78136796176ad6f315230e",
+    "url": "/medium-clone/static/media/banner.bff46db7.png"
+  },
+  {
+    "revision": "1c18765232b93566918a446aebaff920",
+    "url": "/medium-clone/index.html"
+  }
+];
